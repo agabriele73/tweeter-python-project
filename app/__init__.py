ed3a556da3c0e65c1be78ce50bc289ddf3fbbda1
agabriele73 <agabrielejr@gmail.com>
2023-05-02 21:49:25 -0700

    finished phase 1 and phase 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 # !!START
-from flask import Flask
+from flask import Flask, render_template
 from .config import Config
-
+from random import choice
+from .tweets import tweets
 app = Flask(__name__)
 
 app.config.from_object(Config)
 # !!END
+
+@app.route('/')
+def index():
+    random_tweet = choice(tweets)
+    return render_template('index.html', tweet=random_tweet)
+
+
+@app.route('/feed')
+def feed():
+    return render_template('feed.html', tweets=tweets)
--- app/tweets.py
+++ app/tweets.py
@@ -1,4 +1,4 @@
-tweets= [
+tweets = [
     { 
         
         "id": 1,
